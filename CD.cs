ffeae25b461016a196fe1235273c4115f891f683
Peter Blanchfield <zygomy@gmail.com>
2021-10-12 17:04:04 +0100

    Added the basics of the CD Class

---
+++ CD.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateACD_12_10_21
+{
+    class CD
+    {
+        string cdTitle;
+        int tracks;
+        float[] trackLengths;
+        //Constructor - allows a new CD Object to be constructed, that is a new CD instance to be made
+        public CD(string title,int nTracks, float[] tLengths)
+        {
+            cdTitle = title;
+            tracks = nTracks;
+            trackLengths = tLengths;
+        }
+
+        public string Title { get {return cdTitle; }  }
+
+        public int NTracks { get { return tracks; } }
+
+        public float TrackLength(int tn)
+        {
+            return trackLengths[tn];
+        }
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,26 @@ namespace CreateACD_12_10_21
 {
     public partial class Form1 : Form
     {
+        List<CD> cdList;
         public Form1()
         {
             InitializeComponent();
+            cdList = new List<CD>();
+            float[] lot = { 20.25f, 18.8f, 9.6f, 15.6f, 14.5f };
+            CD newCD = new CD("Rachmaninov 2", 5,lot);
+            cdList.Add(newCD);
+            //float f1 = newCD.TrackLength(5);
+            listBox1.Items.Add(newCD.Title);
+            listBox1.Items.Add(newCD.NTracks);
+            for(int i = 0; i < newCD.NTracks; i++)
+            {
+                listBox1.Items.Add(newCD.TrackLength(i));
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
